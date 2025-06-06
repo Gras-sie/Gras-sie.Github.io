8c8aa23fe20e7634e1027f07f35edea2fc8c7f7e
Marius Francois Grassman <marius-jnr@outlook.com>
2025-06-06 13:52:02 +0200

    Setup for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Gras-sie.Github.io/',
   resolve: {
     dedupe: ['react', 'react-dom', 'lucide-react'],
   },
